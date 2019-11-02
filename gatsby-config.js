400122e5d07cc3d52a036737cd0ef36a46032461
Koki Hashimoto <fmfmfmfm@gmail.com>
2019-11-03 01:36:53 +0900

    feat: first version

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `@nullpoo`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
@@ -31,5 +31,6 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
     `gatsby-plugin-preact`,
+    `gatsby-plugin-postcss`,
   ],
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,28 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
+import styles from "./index.module.css";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Koki HASHIMOTO</h1>
+    <p className={`${styles.snsIcon} ${styles.iconLocation}`}>
+      <a href="https://goo.gl/maps/KAG16pAwwSAJVdMB7">Tokyo in Japan</a>
+    </p>
+    <p className={`${styles.snsIcon} ${styles.iconTwitter}`}>
+      <a href="https://twitter.com/nullpoo">@nullpoo</a>
+    </p>
+    <p className={`${styles.snsIcon} ${styles.iconFacebook}`}>
+      <a href="https://www.facebook.com/nullpoo">Koki Hashimoto</a>
+    </p>
+    <p className={`${styles.snsIcon} ${styles.iconLinkedin}`}>
+      <a href="https://www.linkedin.com/in/nullpoo/">Koki Hashimoto</a>
+    </p>
+    <p className={`${styles.snsIcon} ${styles.iconGithub}`}>
+      <a href="https://github.com/nullpoo">nullpoo</a>
+    </p>
   </Layout>
 )
 
